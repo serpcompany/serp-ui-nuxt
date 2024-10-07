22675fa3ae9101efec06273a218a7705dc14970d
devinschumacher <devin@serp.co>
2024-10-06 20:42:05 -0700

    add shadcn-vue in its own folder for reference

--- serp-ui/pages/company/collection.vue
+++ serp-ui/pages/company/collection.vue
@@ -10,6 +10,7 @@
       <section></section>
         <!-- card -->
         
+   
       
       <!-- pagination -->
       <section>
@@ -35,26 +36,9 @@
         </div>
       </section>
       
-      <!-- tag cloud -->
+      <!-- link hub -->
       <section>
-        <div class="container mx-auto">
-          <div class="pt-12">
-            <div>
-              <span class="inline-block py-4 text-3xl font-bold">Related</span>
-            </div>
-          </div>
-
-          <div class="grid grid-cols-1 gap-4 mt-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            <div v-for="tag in tags" :key="tag.slug" class="border-t">
-              <NuxtLink 
-                :to="tag.slug" 
-                class="flex mt-3 font-medium"
-              >
-                {{ tag.name }}
-              </NuxtLink>
-            </div>
-          </div>
-        </div>
+        <LinkHub :tags="tags" />
       </section>
       <!-- tag -->
       
