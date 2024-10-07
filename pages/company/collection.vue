44a2f00a69d7636c153947155c567d02a26ff1cd
devinschumacher <devin@serp.co>
2024-10-06 18:23:48 -0700

    add tag cloud to company collection page

--- pages/company/collection.vue
+++ pages/company/collection.vue
@@ -1,40 +1,61 @@
 <template>
-  <div>
+  <div class="container py-10">
     <!-- hero -->
     <section></section>
     
     <!-- main -->
-     <section>
+    <main>
     
-     <!-- section of cards -->
+      <!-- section of cards -->
       <section></section>
-      <!-- card -->
+        <!-- card -->
         
       
       <!-- pagination -->
       <section>
-        <Pagination v-slot="{ page }" :total="100" :sibling-count="1" show-edges :default-page="2">
-          <PaginationList v-slot="{ items }" class="flex items-center gap-1">
-            <PaginationFirst />
-            <PaginationPrev />
+        <div class="container flex justify-center">
+          <Pagination v-slot="{ page }" :total="100" :sibling-count="1" show-edges :default-page="2">
+            <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+              <PaginationFirst />
+              <PaginationPrev />
 
-            <template v-for="(item, index) in items">
-              <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
-                <Button class="w-10 h-10 p-0" :variant="item.value === page ? 'default' : 'outline'">
-                  {{ item.value }}
-                </Button>
-              </PaginationListItem>
-              <PaginationEllipsis v-else :key="item.type" :index="index" />
-            </template>
+              <template v-for="(item, index) in items">
+                <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
+                  <Button class="w-10 h-10 p-0" :variant="item.value === page ? 'default' : 'outline'">
+                    {{ item.value }}
+                  </Button>
+                </PaginationListItem>
+                <PaginationEllipsis v-else :key="item.type" :index="index" />
+              </template>
 
-            <PaginationNext />
-            <PaginationLast />
-          </PaginationList>
-        </Pagination>
+              <PaginationNext />
+              <PaginationLast />
+            </PaginationList>
+          </Pagination>
+        </div>
       </section>
       
       <!-- tag cloud -->
-      <section></section>
+      <section>
+        <div class="container mx-auto">
+          <div class="pt-12">
+            <div>
+              <span class="inline-block px-3 py-2 text-3xl font-bold">Related</span>
+            </div>
+          </div>
+
+          <div class="grid grid-cols-1 gap-4 mt-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div v-for="tag in tags" :key="tag.slug" class="border-t">
+              <NuxtLink 
+                :to="tag.slug" 
+                class="flex mt-2 font-medium"
+              >
+                {{ tag.name }}
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </section>
       <!-- tag -->
       
       <!-- article section -->
@@ -45,12 +66,120 @@
           <!-- faqs -->
            <section></section>
             <!-- faq -->
-</section>
+    </main>
   </div>
 </template>
 
 
 <script setup lang='ts'>
+const tags = [
+  {
+    name: "Email Marketing",
+    slug: "email-marketing"
+  },
+  {
+    name: "SEO",
+    slug: "seo"
+  },
+  {
+    name: "PPC",
+    slug: "ppc"
+  },
+  {
+    name: "Python",
+    slug: "python"
+  },
+  {
+    name: "Marketing",
+    slug: "marketing"
+  },
+  {
+    name: "Sales",
+    slug: "sales"
+  },
+  {
+    name: "Lead Generation",
+    slug: "lead-generation"
+  },
+  {
+    name: "CRM",
+    slug: "crm"
+  },
+  {
+    name: "Project Management",
+    slug: "project-management"
+  },
+  {
+    name: "Image Generation",
+    slug: "image-generation"
+  },
+  {
+    name: "Spreadsheets",
+    slug: "spreadsheets"
+  },
+  {
+    name: "Calendar Apps",
+    slug: "calendar-apps"
+  },
+  {
+    name: "Blueberry Pie",
+    slug: "blueberry-pie"
+  },
+  {
+    name: "Borscht",
+    slug: "borscht"
+  },
+  {
+    name: "Breakfast and Brunch",
+    slug: "breakfast-and-brunch"
+  },
+  {
+    name: "Breakfast Burritos",
+    slug: "breakfast-burritos"
+  },
+  {
+    name: "Breakfast Casseroles",
+    slug: "breakfast-casseroles"
+  },
+  {
+    name: "Breakfast Potatoes",
+    slug: "breakfast-potatoes"
+  },
+  {
+    name: "Breakfast Stratas",
+    slug: "breakfast-stratas"
+  },
+  {
+    name: "Broccoli Salads",
+    slug: "broccoli-salads"
+  },
+  {
+    name: "Brownies",
+    slug: "brownies"
+  },
+  {
+    name: "Bruschetta",
+    slug: "bruschetta"
+  },
+  {
+    name: "Buffalo Chicken Dips",
+    slug: "buffalo-chicken-dips"
+  },
+  {
+    name: "Buffalo Chicken Wings",
+    slug: "buffalo-chicken-wings"
+  },
+  {
+    name: "Burritos",
+    slug: "burritos"
+  },
+  {
+    name: "Butternut Squash Soups",
+    slug: "butternut-squash-soups"
+  }
+];
+
+
 const companies = [
   {
     id: 1,
