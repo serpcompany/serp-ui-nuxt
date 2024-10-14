455fe9c05cb68688ba3679499074123b2576f3a2
devinschumacher <devin@serp.co>
2024-10-14 11:18:37 -0700

    mend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,30 @@
-// eslint.config.mjs
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
+import vitest from '@vitest/eslint-plugin';
 
-export default withNuxt({
-  rules: {
-    'vue/block-order': ['error', { order: ['template', 'script', 'style'] }],
-    'vue/no-setup-props-reactivity-loss': 'error',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'no-console': 'warn',
-    'no-unused-vars': 'off',
-    'prefer-const': 'warn',
+export default withNuxt([
+  {
+    files: ['tests/**'],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      'vitest/max-nested-describe': ['error', { max: 3 }],
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
+      },
+    },
   },
-});
+  {
+    rules: {
+      'vue/block-order': ['error', { order: ['template', 'script', 'style'] }],
+      'vue/no-setup-props-reactivity-loss': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'no-unused-vars': 'off',
+      'prefer-const': 'warn',
+    },
+  },
+]);
