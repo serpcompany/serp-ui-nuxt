cda10ac347178db94a2cab91abe2f7c98cb8cea3
devinschumacher <devin@serp.co>
2024-10-16 19:14:22 -0700

    setup test config

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-/** @type {import('vite').UserConfig} */
-export default defineConfig({
-  test: {
-    globals: true,
-    setupFiles: ['./tests/setup.js'], // tells vitest to run this file before running any assertions/tests in any *.tests.js files.
-  },
-});
